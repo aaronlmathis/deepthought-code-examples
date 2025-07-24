b10323cc16a2a94125a203983668a047564b0457
Aaron Mathis <aaron.mathis@gmail.com>
2025-07-24 13:01:07 -0500

    Adding terraform cloud backend

---
+++ infrastructure-as-code/introduction-to-terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  cloud {
+    organization = "Deep-Thought"
+    workspaces {
+      name = "homelab"
+    }
+  }
+}
--- infrastructure-as-code/introduction-to-terraform/locals.tf
+++ infrastructure-as-code/introduction-to-terraform/locals.tf
@@ -5,10 +5,10 @@ locals {
   # The final map of nodes to create. This uses the user-provided `var.nodes` if it's not null,
   # otherwise it falls back to a default configuration.
   nodes = var.nodes != null ? var.nodes : {
-    "master-1" = { memory = 2048, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.100" }
-    "worker-1" = { memory = 2048, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.101" }
-    "worker-2" = { memory = 2048, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.102" }
-    "worker-3" = { memory = 2048, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.103" }
-    "worker-4" = { memory = 2048, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.104" }
+    "master-1" = { memory = 4096, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.100" }
+    "worker-1" = { memory = 4096, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.101" }
+    "worker-2" = { memory = 4096, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.102" }
+    "worker-3" = { memory = 4096, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.103" }
+    "worker-4" = { memory = 4096, vcpu = 2, disk_size = 20 * local.gb, ip = "192.168.122.104" }
   }
 }
